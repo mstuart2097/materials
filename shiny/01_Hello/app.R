fc74e156eab0f400cb92229abcf91d7943b65a90
Heike Hofmann <hofmann@iastate.edu>
2017-04-06 11:52:34 -0500

    plotly and ggvis

--- shiny/01_Hello/app.R
+++ shiny/01_Hello/app.R
@@ -3,17 +3,20 @@ require(ggplot2)
 require(dplyr)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(    
+ui <- fluidPage(
     # Application title
     titlePanel("Hello Shiny World!"),
-    
+
     # Sidebar with a slider input for the number of bins
 	sidebarPanel(
 	    sliderInput("num",
 		        "Number of Samples:",
 		        min = 1,
 		        max = 5000,
-		        value = 2500)
+		        value = 2500),
+	    numericInput("mean", "Mean: ", value=0),
+	    numericInput("sd", "Standard deviation:",
+	                 value = 1, min=0.0001)
 	),
 
 	# Show a plot of the generated distribution
@@ -26,7 +29,7 @@ ui <- fluidPage(
 server <- function(input, output) {
     output$hist <- renderPlot({
         dist <- rnorm(n = input$num)
-        gg <- data.frame(dist) %>% 
+        gg <- data.frame(dist) %>%
           ggplot(aes(x = dist)) + geom_histogram(binwidth = 0.25) +
           xlim(c(-10,10))
         print(gg)
