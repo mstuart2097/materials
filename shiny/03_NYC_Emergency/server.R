8e1a8fd600fa4047fb7c73e4e5b2a36e1b91cef0
Heike Hofmann <hofmann@iastate.edu>
2017-04-10 10:28:14 -0500

    crosstalk examples

--- shiny/03_NYC_Emergency/server.R
+++ shiny/03_NYC_Emergency/server.R
@@ -5,12 +5,12 @@ library(dplyr)
 nyc <- read.csv("nyc_emergency.csv")
 
 shinyServer(function(input, output) {
-    
+
     nyc_subset <- reactive({
         nyc %>%
             filter(Incident.Type == input$incident_type)
     })
-    
+
     output$crime <- renderPlot({
         ggplot(data = nyc_subset(), aes(x = Borough, fill = Borough)) +
             geom_bar(stat = "count") +
@@ -18,4 +18,10 @@ shinyServer(function(input, output) {
             ggtitle(paste("Number of", input$incident_type, "Reports by Borough"))
     })
 
+    output$location <- renderPlot({
+      ggplot(data = nyc_subset(), aes(x = Longitude, y = Latitude)) +
+        geom_point(aes(colour = Borough)) +
+        theme_bw() +
+        ggtitle("Locations of incidents")
+    })
 })
--- shiny/03_NYC_Emergency/ui.R
+++ shiny/03_NYC_Emergency/ui.R
@@ -3,14 +3,17 @@ library(shiny)
 nyc <- read.csv("nyc_emergency.csv", stringsAsFactors = FALSE)
 
 shinyUI(fluidPage(
-    
+
     titlePanel("NYC Crime Data"),
-    
+
 	sidebarPanel(
 	    selectInput("incident_type", "Incident Type", choices = sort(unique(nyc$Incident.Type)), selected = "Fire-3rd Alarm")
 	),
 
 	mainPanel(
-	    plotOutput("crime")
+	  tabsetPanel(
+	    tabPanel("Barchart", plotOutput("crime")),
+	    tabPanel("Scatterplot of Locations", plotOutput("location"))
+	  )
 	)
 ))
